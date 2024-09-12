e9aa2906a8d96d9d058805fa7ecdfccf75acf6cb
wst <hedyhefty@outlook.com>
2024-09-12 16:32:24 +0800

    add discovery client

--- src/main/java/com/me/llmservice/LLMApplication.java
+++ src/main/java/com/me/llmservice/LLMApplication.java
@@ -2,8 +2,10 @@ package com.me.llmservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LLMApplication {
     public static void main(String[] args) {
         SpringApplication.run(LLMApplication.class,args);
